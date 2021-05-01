a35e7515caac3e10bd69c61912245291d0bde3bb
VitalinaShershan <vshershan@gmail.com>
2021-05-01 13:27:07 +0300

    Planets

---
+++ src/com/collection/Main.java
@@ -0,0 +1,15 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> planets= new ArrayList<>();
+        planets.add("Sun");
+        planets.add("Earth");
+        planets.add("Mars");
+        System.out.println(planets);
+    }
+}
